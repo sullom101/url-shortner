fc2edbda3ad3cddb33d06fb4aaa87c940386f061
sullom101 <mayow.sullom@gmail.com>
2019-12-03 19:49:25 +0800

    track screen width and pass it to child components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Header from './Components/Header/Header';
+import Bannar from './Components/Bannar/Bannar';
+import '@fortawesome/fontawesome-free/css/all.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = { 
+      width: window.innerWidth
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+    console.log(this.state.width);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header width={this.state.width} /> 
+        <Bannar/>
+      </div>
+    );
+  }
 }
 
 export default App;
