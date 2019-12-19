7d7a70196018b66b4464ec227d7db7a172d1641f
sullom101 <mayow.sullom@gmail.com>
2019-12-19 12:49:09 +0800

    App with Minimalist css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import './App.css';
 import Header from './Components/Header/Header';
-import Bannar from './Components/Bannar/Bannar';
-import '@fortawesome/fontawesome-free/css/all.css';
+import Bannar from './Components/Bannar/Index';
+import Shorten from './Components/Shorten/Shorten';
+import Services from './Components/Services/Index'
+import Boost from './Components/Boost/Boost';
+import Footer from './Components/Footer/Index';
 
 class App extends Component { 
   constructor(props) {
     super(props);
     this.state = { 
-      width: window.innerWidth
+      width: 0
     };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
@@ -31,7 +34,14 @@ class App extends Component {
     return (
       <div className="App">
         <Header width={this.state.width} /> 
-        <Bannar/>
+        <Bannar width={this.state.width}/>
+
+        <main style={{backgroundColor:'#f0f1f6', position:'relative', zIndex:0}}>
+        <Shorten width={this.state.width}/>
+        <Services width={this.state.width}/>
+        <Boost />
+        </main>
+        <Footer width={this.state.width} />
       </div>
     );
   }
